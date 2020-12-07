a5d34f3905e74df2eddb51d9fa63a17db8c71884
budman-dev <75591413+budman-dev@users.noreply.github.com>
2020-12-06 21:15:55 -0500

    added files

---
+++ Data Recovery/Assets/dragScript.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class dragScript : MonoBehaviour {
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    Animator anim;
+    bool frontSide = true;
+    float clickCounter = 0;
+    bool counting = false;
+    public bool tiny;
+    public bool exit;
+    bool wasTiny = false;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    void OnMouseDown()
+    {
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+  
+   
+    void OnMouseDrag()
+    {
+        clickCounter += Time.deltaTime;
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        if (tiny)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z + 10));
+            wasTiny = true;
+        } else if (wasTiny)
+        {
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z + 10));
+        }
+        else
+        {
+            transform.position = curPosition;
+        }
+        
+        
+    }
+    void OnMouseUp()
+    {
+        wasTiny = false;
+        if (clickCounter <= .12f)
+        {
+            if (frontSide)
+            {
+                frontSide = false;
+            }
+            else
+            {
+                frontSide = true;
+            }
+        }
+        clickCounter = 0;
+    }
+    private void Update()
+    {
+        anim.SetBool("Front", frontSide);
+        anim.SetBool("Tiny", tiny);
+
+        Vector2 S = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
+        gameObject.GetComponent<BoxCollider2D>().size = S;
+
+        if (Input.mousePosition.y > Screen.height * .1 && exit)
+        {
+            tiny = false;
+            exit = false;
+        }
+    }
+    
+}
---
+++ Data Recovery/Assets/itemBar.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class itemBar : MonoBehaviour {
+
+    
+
+    float speed = 1;
+    bool mouseTime = false;
+    public bool mouseLow = false;
+    public dragScript dragScript;
+    public GameObject item;
+    void Start () {
+
+        transform.position = new Vector3(0.0f, -5.35f, 0f);
+        
+    }
+
+    //  private void OnMouseOver()
+    // {
+    //     if (transform.position.y <= -4.5f)
+    //     {
+    //         transform.Translate(Vector3.up * Time.deltaTime * 4, Space.World);
+    //         mouseTime = true;
+    //     } 
+    //     
+    // }
+    // private void OnMouseExit()
+    // {
+    //      mouseTime = false;
+    // }
+    //-4.2752
+    // .07248
+    void Update()
+    {
+        if (Input.mousePosition.y <= Screen.height * .1)
+        {
+            mouseLow = true;
+            if (transform.position.y <= -4.5f)
+            {
+                transform.Translate(Vector3.up * Time.deltaTime * 4, Space.World);
+            }
+        }
+        else if (transform.position.y > -5.35f)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * 4, Space.World);
+            
+        }
+        if (Input.mousePosition.y > Screen.height*.1)
+        {
+            mouseLow = false;
+        }
+        
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Item" && mouseLow)
+        {
+            
+            dragScript = collision.gameObject.GetComponent<dragScript>();
+            dragScript.tiny = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D collision2)
+    {
+       if (collision2.gameObject.tag == "Item")
+        {
+            dragScript = collision2.gameObject.GetComponent<dragScript>();
+            dragScript.exit = true;
+                // dragScript.tiny = false;
+        }
+    }
+
+}
